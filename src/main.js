e7fd1c9241cfc4d9bc58f5d5b483d2189d4fa9a8
Mari Trtnik <maripartan@gmail.com>
2020-05-15 22:55:05 +0300

    add navigation bar

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import { MdToolbar, MdButton, MdIcon, MdTabs } from 'vue-material/dist/components'
 import App from './App.vue'
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default.css';
 
+Vue.use(VueRouter)
+Vue.use(MdToolbar)
+Vue.use(MdButton)
+Vue.use(MdIcon)
+Vue.use(MdTabs)
 Vue.config.productionTip = false
 
 new Vue({
--- tuntikirjanpito.md
+++ tuntikirjanpito.md
@@ -1,5 +1,6 @@
 # Tuntikirjanpito
 
-| Päivä | Aika | mitä tein  |
+| Päivä | Aika | Mitä |
 | :----:|:-----| :-----|
-| 11.5.  | 1    | Projektin luominen, käyttöliittymän suunnittelu |
+| 11.5. |   1  | Projektin luominen, käyttöliittymän suunnittelu |
+| 15.5. |   1  | Navigaatiopalkki |
